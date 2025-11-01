b6da60a31a389c0cdbf50da36f760deceb39553a
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-11-01 21:26:03 +0530

    data type conversions

---
+++ 03_data_type_conversions.js
@@ -0,0 +1,24 @@
+let score =undefined
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" number
+// "33ac" type of number  || actual value NaN hogi
+// true -> 1 | false -> 0
+// null -> 0 || string
+// undefined ->Nan|| type string
+
+
+
+
+let isLoggedIn = Boolean(1)
+
+
+let someNum = 333
+let StringNumber = String(someNum)
+console.log(StringNumber);
+console.log(typeof StringNumber);
