f4ef82e6c6d1fe393f080d05dc44c2e3b64a6a2f
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-11-01 21:02:37 +0530

    first lecture

---
+++ 01_let_var_const.js
@@ -0,0 +1,17 @@
+console.log("Hitesh");
+
+const accountId = 144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId = 696969   Assignment to constant variable || Not Allowed
+// console.log(accountId);
+
+
+accountEmail="bawa@gmail.com"
+accountPassword="2121"
+accountCity="Banglore"
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
