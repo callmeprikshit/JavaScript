e06d0c0b5dffceac06e2f9d98453637245217814
Prikshitsharmacodes <prikshitsharma.codes@gmail.com>
2025-11-01 21:14:56 +0530

    learnt about various data-types

---
+++ 02_DataTypes_EcmaScript.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer one
+
+// alert(3+3)// we are using nodejs , not browser
+
+console.log(3+3);
+
+//Data-Types
+//Number
+//BigInt
+//String
+// Null -> Standalone value
+// undefined ->
+// symbol -> unique
+
+
+
+//Object
+
+console.log(typeof Object);// function
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
